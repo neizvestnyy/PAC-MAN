5f0efb16a591cb483314bde825b9bc583aa767e4
gouv0002 <guillaume.gouverneur@etudiant.univ-reims.fr>
2017-11-21 00:57:18 +0100

    Modified fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = (VBox)FXMLLoader.load(getClass().getResource("rullo.fxml"));
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("pac-man.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
