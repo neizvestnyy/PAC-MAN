b1ddb7aa8fef7b770c0796b8c60926ee6563abb4
gouv0002 <guillaume.gouverneur@etudiant.univ-reims.fr>
2017-11-21 00:58:24 +0100

    Added constants class

---
+++ src/application/Constants.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.scene.image.Image;
+
+public class Constants {
+
+	public static final int BOARD_WIDTH = 28;
+	public static final int BOARD_HEIGHT = 31;
+	public static final Image WALL_PICTURE = new Image("wall.png");
+	public static final Image VOID_PICTURE = new Image("void.png");
+	public static final Image DOOR_PICTURE = new Image("door.png");
+	public static final Image DOT_PICTURE = new Image("dot.png");
+	public static final Image ENERGIZER_PICTURE = new Image("energizer.png");
+	public static final Image CHERRY_PICTURE = new Image("cherry.png");
+	public static final Image STRAWBERRY_PICTURE = new Image("strawberry.png");
+	public static final Image PEACH_PICTURE = new Image("peach.png");
+	public static final Image APPLE_PICTURE = new Image("apple.png");
+	public static final Image GRAPE_PICTURE = new Image("grape.png");
+	public static final Image GALAXIAN_PICTURE = new Image("galaxian.png");
+	public static final Image BELL_PICTURE = new Image("bell.png");
+	public static final Image KEY_PICTURE = new Image("key.png");
+
+}
