1449738ab98111349f912a8f44879bfbd4408031
gouv0002 <guillaume.gouverneur@etudiant.univ-reims.fr>
2017-11-21 23:54:40 +0100

    Modified

--- src/application/PMController.java
+++ src/application/PMController.java
@@ -1,9 +1,22 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
 public class PMController {
 	
+	@FXML
+	StackPane stackPane;
+	
+	private view.Board view;
+	private model.Board model;
+	
     public void initialize() {
-    	
+    	this.view = new view.Board();
+    	this.stackPane.getChildren().add(this.view);
+    	this.model = new model.Board();
+    	this.view.setModel(this.model);
+    	this.model.startGame();
     }
 
 }
